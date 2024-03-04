341bcbef992c5de037bc970e016a87e6b083270b
KurtSchwimmbacher <231002@virtualwindow.co.za>
2024-03-04 09:21:19 +0200

    style header buttons
    style header buttons and adjust nav bar position

--- dndstats/src/pages/Dashboard.js
+++ dndstats/src/pages/Dashboard.js
@@ -2,14 +2,23 @@ import "../App.css";
 import Navbar from "../components/Navbar";
 import HeaderBtn from "../components/HeaderBtn";
 
+// import bootstap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Dashboard(){
     return(
         <>
             <Navbar />
             <h1 className="dashboard-title">Adventurer's Almanac</h1>
-            <HeaderBtn where="Compare" />
-            <HeaderBtn where="ViewSingle" />
-            <HeaderBtn where = "ViewTimeline" />
+            <Container fluid="md" className="header-con">
+                <Row>
+                    <Col><HeaderBtn where="Compare" /></Col>
+                    <Col><HeaderBtn where="ViewSingle" /></Col>
+                    <Col><HeaderBtn where = "ViewTimeline" /></Col>                    
+                </Row>
+            </Container>
         </>
     );
 }
