9c508224709ccabf85aeb9fd8aacf3930e273b42
KurtSchwimmbacher <231002@virtualwindow.co.za>
2024-03-07 11:04:28 +0200

    fix sticky nav bar
    make nav bar sticky

--- dndstats/src/pages/Dashboard.js
+++ dndstats/src/pages/Dashboard.js
@@ -18,30 +18,35 @@ function Dashboard(){
 
     return(
         <>
-            <Navbar />
-            <h1 className="dashboard-title">Adventurer's Almanac</h1>
-            <p className="dashboard-subtitle">
-                Adventurer’s Almanac takes data from the open5e API to 
-                allow for comparisons between different Dungeons and Dragons
-                classes, and therefore different Baldur’s Gate 3 Classes 
-                as well. We use the data from the API to formulate graphs to 
-                compare several aspects of the classes, such as Hit Points 
-                and spells! 
-            </p>
-            <Container fluid="md" className="header-con">
-                <Row>
-                    <Col><HeaderBtn where="Compare" /></Col>
-                    <Col><HeaderBtn where="BG3" /></Col>
-                    <Col><HeaderBtn where = "ViewTimeline" /></Col>                    
-                </Row>
+        <div className="dash-layout">
+            <div className="side-bar">
+                <Navbar />
+            </div>
+            <div className="page-content">
+                    
+                <h1 className="dashboard-title">Adventurer's Almanac</h1>
+                <p className="dashboard-subtitle">
+                    Adventurer’s Almanac takes data from the open5e API to 
+                    allow for comparisons between different Dungeons and Dragons
+                    classes, and therefore different Baldur’s Gate 3 Classes 
+                    as well. We use the data from the API to formulate graphs to 
+                    compare several aspects of the classes, such as Hit Points 
+                    and spells! 
+                </p>
+                <Container fluid="md" className="header-con">
+                    <Row>
+                        <Col><HeaderBtn where="Compare" /></Col>
+                        <Col><HeaderBtn where="BG3" /></Col>
+                        <Col><HeaderBtn where = "ViewTimeline" /></Col>                    
+                    </Row>
 
-                <Row>
-                <BentoGrid />
-                </Row>
-            </Container>
-           
-           
+                    <Row>
+                    <BentoGrid />
+                    </Row>
+                </Container>
+            </div>
             
+        </div>
 
         </>
     );
