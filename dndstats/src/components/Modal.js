88f813c6ca1b3d14dd198ccd0851248aa3147b03
KurtSchwimmbacher <231002@virtualwindow.co.za>
2024-03-28 16:53:38 +0200

    adjust modal to have onclick working
    make the button to choose class work when outputting selected class. Update how button image is loaded so that it changes to represent chosen class

--- dndstats/src/components/Modal.js
+++ dndstats/src/components/Modal.js
@@ -1,69 +1,71 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import "../App.css";
 import axios from 'axios';
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
+import defaultModalButton from "../assets/Icons/plus-circle-svgrepo-com.svg";
+
 import Card from 'react-bootstrap/Card';
-
-
 
 function ModalComp() {
   const [show, setShow] = useState(false);
+  const [classes, setClasses] = useState([]);
+  // State to store the selected class image
+  const [selectedClassImage, setSelectedClassImage] = useState(); 
+
+  useEffect(() => {
+    const url = "https://www.dnd5eapi.co/api/classes";
+    axios.get(url).then((response)=>{
+      setClasses(response.data.results);
+    });
+  }, []);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [active, setActive] = useState("");  
-  const [classes,setClasses] = useState([]);
-
-    useEffect(() => {
-        const url = "https://www.dnd5eapi.co/api/classes";
-        axios.get(url).then((response)=>{
-            setClasses(response.data.results);
-        })
-    }, [active]);
-
-    const handleCardClick = (className) => {
-      // console.log(className);
-      // const timeOutClose = setTimeout(()=>{
-      //   handleClose()
-      // },400)
-    };
-
-
+  const handleCardClick = (className, imageUrl) => {
+    // Log the class name when the card is clicked
+    console.log(className); 
+    // Set the selected class image
+    setSelectedClassImage(imageUrl); 
+    handleClose(); 
+  };
 
   return (
     <>
-      <Button onClick={handleShow} className='slct-class-btn' id='modalOpenBtn'></Button>
+      <Button onClick={handleShow} className='slct-class-btn'id='modalOpenBtn' style={{background: "#FAF9F6", border: "none"}}>
+  <img 
+    src={selectedClassImage ? selectedClassImage : defaultModalButton} 
+    alt="Selected Class"
+    style={{ 
+      width: '300px', 
+      height: '300px', 
+      objectFit: 'cover', 
+      border: 'none'
+    }} 
+  />
+</Button>
 
-      <Modal  show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Choose a Class</Modal.Title>
         </Modal.Header>
-        <Modal.Body onClick={(e)=>{
-          console.log(e.target.value)
-        }}>
-            {classes 
-            ? classes.map((item) => {
-                return(
-                  <Card key={item.name} className='modal-card'  onClickCapture={handleCardClick(item.name)} >
-                    <Card.Img variant="top" src={require(`../assets/images/${item.name}.png`)} />
-                      <Card.Body>
-                          <Card.Title>{item.name}</Card.Title>
-                      </Card.Body>
-                  </Card>
-                )
-            })
-            : null}
+        <Modal.Body>
+          {classes ? (
+            classes.map((item) => (
+              <Card key={item.name} className='modal-card' onClick={() => handleCardClick(item.name, require(`../assets/images/${item.name}.png`))}>
+                <Card.Img variant="top" src={require(`../assets/images/${item.name}.png`)} />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            ))
+          ) : null}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
         </Modal.Footer>
       </Modal>
     </>
